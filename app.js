97bc8ad8a1c785dc1910ee2e6cd46b51fd2b76d3
Nakotey <ebenezer.tetteh@amalitech.org>
2021-08-20 11:21:44 +0000

    changes made in db.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.json()); // to grab message body with req.body encoded as JSON
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.urlencoded({extended:false}));
 app.use('/', message);
-app.use(template);
+app.use('/', template);
 
 // Init africastalking
 const credentials = {
--- models/db.js
+++ models/db.js
@@ -1,11 +1,12 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user: "postgres",
-    password: "Entai",
-    host: "localhost",
+    user: "mjbzaofxwhvcgd",
+    password: "ce2c575df3522b0d0a2d4339eac8df4afcae6f87d589f43cf76918bbeb8dbfb1",
+    host: "1@ec2-54-156-60-12.compute-1.amazonaws.com",
     port: 5432,
-    database: "smssender"
+    database: "d23uigunhfndik",
+
 });
 
 module.exports = pool;
