c2122a8b514fe9ba95b5c4304b578528f1bdc1c3
Nakotey <ebenezer.tetteh@amalitech.org>
2021-08-20 11:58:11 +0000

    altered host in db.js

--- models/db.js
+++ models/db.js
@@ -3,7 +3,7 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
     user: "mjbzaofxwhvcgd",
     password: "ce2c575df3522b0d0a2d4339eac8df4afcae6f87d589f43cf76918bbeb8dbfb1",
-    host: "1@ec2-54-156-60-12.compute-1.amazonaws.com",
+    host: "ec2-54-156-60-12.compute-1.amazonaws.com",
     port: 5432,
     database: "d23uigunhfndik",
 
